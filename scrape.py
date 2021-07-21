e84aa052a1291149f1e0bfc3502b42813cdf794d
christajosephina <86114542+christajosephina@users.noreply.github.com>
2021-07-21 12:30:58 +0200

    scraping with selenium

--- scrape.py
+++ scrape.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[69]:
+# In[1]:
+
+
+pip install selenium
+
+
+# In[2]:
+
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import pandas as pd
+
+
+# In[3]:
+
+
+driver = webdriver.Chrome('/Users/christakoeyvoets/Downloads/chromedriver')
+
+
+# In[4]:
 
 
 from bs4 import BeautifulSoup
@@ -9,51 +29,89 @@ import requests
 from urllib.parse import urlparse
 import json
 import pandas as pd
-
-
-# In[2]:
-
-
-url = 'https://www.booking.com/searchresults.nl.html?label=gen173nr-1FCAEoggI46AdIM1gEaDuIAQGYARy4AQfIAQzYAQHoAQH4AQuIAgGoAgO4AuaDsocGwAIB0gIkN2UzYTE4ZmQtYTlhZC00NjY1LWFmMTctNzY1NDljMjkyMWJj2AIG4AIB&sid=e5cb477f66581f1895119ed9df6cb6b2&sb=1&sb_lp=1&src=index&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Findex.nl.html%3Flabel%3Dgen173nr-1FCAEoggI46AdIM1gEaDuIAQGYARy4AQfIAQzYAQHoAQH4AQuIAgGoAgO4AuaDsocGwAIB0gIkN2UzYTE4ZmQtYTlhZC00NjY1LWFmMTctNzY1NDljMjkyMWJj2AIG4AIB%3Bsid%3De5cb477f66581f1895119ed9df6cb6b2%3Bsb_price_type%3Dtotal%26%3B&ss=Hamburg%2C+Hansestadt+Hamburg%2C+Duitsland&is_ski_area=&checkin_year=&checkin_month=&checkout_year=&checkout_month=&group_adults=2&group_children=0&no_rooms=1&b_h4u_keep_filters=&from_sf=1&search_pageview_id=100a7df32af6001a&ac_suggestion_list_length=5&ac_suggestion_theme_list_length=0&ac_position=0&ac_langcode=nl&ac_click_type=b&dest_id=-1785434&dest_type=city&iata=HAM&place_id_lat=53.549999&place_id_lon=10&search_pageview_id=100a7df32af6001a&search_selected=true&ss_raw=hamburg'
-
-
-# # Parsing it (Soma's way)
-
-# In[4]:
-
-
-response = requests.get(url)
-doc = BeautifulSoup(response.text, 'html.parser')
+from datetime import date
+import datetime as datetime
 
 
 # In[5]:
 
 
-#doc
+today = date.today() 
+year = today.year
+day = today.day
+month = today.month
 
 
-# In[62]:
+# In[6]:
 
 
-names = doc.find_all('span', itemprop='name')
+tomorrow = date.today() + datetime.timedelta(days=1)
+yeartm = tomorrow.year
+daytm = tomorrow.day
+monthtm = tomorrow.month
 
 
-# In[63]:
+# In[7]:
 
 
-prices = doc.find_all(class_='bui-price-display__value bui-f-color-constructive')
+year = str(year)
+day = str(day)
+month = str(month)
+yeartm = str(yeartm)
+daytm = str(daytm)
+monthtm = str(monthtm)
 
 
-# In[65]:
+# In[8]:
+
+
+urltime = "https://www.booking.com/searchresults.nl.html?aid=376373&label=Boekings-nl-Nmlwg4ZjCgLYTSLOHsJrjgS267724993818%3Apl%3Ata%3Ap1%3Ap22.563.000%3Aac%3Aap%3Aneg%3Afi%3Atikwd-65526620%3Alp9043480%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YfpWGnRw6lOGtdKYocc2a8Y&sid=552e2254bbf42007c1fa7f86a7c26c19&sb=1&sb_lp=1&src=index&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Findex.nl.html%3Faid%3D376373%3Blabel%3DBoekings-nl-Nmlwg4ZjCgLYTSLOHsJrjgS267724993818%253Apl%253Ata%253Ap1%253Ap22.563.000%253Aac%253Aap%253Aneg%253Afi%253Atikwd-65526620%253Alp9043480%253Ali%253Adec%253Adm%253Appccp%253DUmFuZG9tSVYkc2RlIyh9YfpWGnRw6lOGtdKYocc2a8Y%3Bsid%3D552e2254bbf42007c1fa7f86a7c26c19%3Bsb_price_type%3Dtotal%26%3B&ss=Hamburg&is_ski_area=0&ssne=Hamburg&ssne_untouched=Hamburg&dest_id=-1785434&dest_type=city&checkin_year=" + year + "&checkin_month=" + month + "&checkin_monthday=" + day + "&checkout_year=" + yeartm + "&checkout_month=" + monthtm + "&checkout_monthday=" + daytm + "&group_adults=1&group_children=0&no_rooms=1&b_h4u_keep_filters=&from_sf=1"
+
+
+# In[46]:
+
+
+driver.get(urltime)
+
+
+# In[47]:
+
+
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+
+
+# In[70]:
+
+
+names = []
+prices = []
+
+
+# In[71]:
+
+
+for item in soup.findAll('span', {'class': 'sr-hotel__name'}):
+        names.append(item.get_text(strip=True))
+
+
+# In[74]:
+
+
+for item in soup.findAll('div', {'class':'bui-price-display__value prco-inline-block-maker-helper'}):
+    prices.append(item.get_text(strip=True))
+
+
+# In[77]:
 
 
 for price, name in zip(prices, names):
     print('---')
-    print(name.text)
-    print(price.text)
+    print(name)
+    print(price)
 
 
-# In[73]:
+# In[78]:
 
 
 rows = []
@@ -63,28 +121,28 @@ for price, name in zip(prices, names):
     print('---')
     #print(name.text)
     #print(price.text)
-    row['Hotel'] = name.text
-    row['Price'] = price.text
+    row['Hotel'] = name
+    row['Price'] = price
     print(row)
     rows.append(row)
 
 
-# In[90]:
+# In[79]:
 
 
 df = pd.DataFrame(rows)
 df
 
 
-# In[105]:
+# In[81]:
 
 
-df=df.replace(to_replace = '€', value ='')
-df
+df['Date']=today
 
 
-# In[106]:
+# In[85]:
 
 
-df.to_csv("hamburghotelprices.csv")
+with open("hamburghotelprices.csv", 'a') as f:
+    df.to_csv(f, header=f.tell()==0)
 
