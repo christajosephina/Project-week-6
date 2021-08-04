3d5e7f4a289aa2d767a12ac8cc34064d10e25342
christajosephina <86114542+christajosephina@users.noreply.github.com>
2021-08-04 21:05:46 +0200

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[86]:
+# In[24]:
 
 
 #pip install selenium
+#!pip install webdriver_manager
 
 
-# In[87]:
+# In[35]:
 
 
 import selenium
 
 
-# In[2]:
+# In[36]:
 
 
 from selenium import webdriver
@@ -21,13 +22,17 @@ from selenium.webdriver.common.keys import Keys
 import pandas as pd
 
 
-# In[3]:
+# In[37]:
 
 
-driver = webdriver.Chrome('/Users/christakoeyvoets/Downloads/chromedriver')
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
 
-# In[4]:
+# In[38]:
 
 
 from bs4 import BeautifulSoup
@@ -39,7 +44,7 @@ from datetime import date
 import datetime as datetime
 
 
-# In[5]:
+# In[39]:
 
 
 today = date.today() 
@@ -48,7 +53,7 @@ day = today.day
 month = today.month
 
 
-# In[6]:
+# In[40]:
 
 
 tomorrow = date.today() + datetime.timedelta(days=1)
@@ -57,7 +62,7 @@ daytm = tomorrow.day
 monthtm = tomorrow.month
 
 
-# In[7]:
+# In[41]:
 
 
 year = str(year)
@@ -68,47 +73,47 @@ daytm = str(daytm)
 monthtm = str(monthtm)
 
 
-# In[8]:
+# In[42]:
 
 
 urltime = "https://www.booking.com/searchresults.nl.html?aid=376373&label=Boekings-nl-Nmlwg4ZjCgLYTSLOHsJrjgS267724993818%3Apl%3Ata%3Ap1%3Ap22.563.000%3Aac%3Aap%3Aneg%3Afi%3Atikwd-65526620%3Alp9043480%3Ali%3Adec%3Adm%3Appccp%3DUmFuZG9tSVYkc2RlIyh9YfpWGnRw6lOGtdKYocc2a8Y&sid=552e2254bbf42007c1fa7f86a7c26c19&sb=1&sb_lp=1&src=index&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Findex.nl.html%3Faid%3D376373%3Blabel%3DBoekings-nl-Nmlwg4ZjCgLYTSLOHsJrjgS267724993818%253Apl%253Ata%253Ap1%253Ap22.563.000%253Aac%253Aap%253Aneg%253Afi%253Atikwd-65526620%253Alp9043480%253Ali%253Adec%253Adm%253Appccp%253DUmFuZG9tSVYkc2RlIyh9YfpWGnRw6lOGtdKYocc2a8Y%3Bsid%3D552e2254bbf42007c1fa7f86a7c26c19%3Bsb_price_type%3Dtotal%26%3B&ss=Hamburg&is_ski_area=0&ssne=Hamburg&ssne_untouched=Hamburg&dest_id=-1785434&dest_type=city&checkin_year=" + year + "&checkin_month=" + month + "&checkin_monthday=" + day + "&checkout_year=" + yeartm + "&checkout_month=" + monthtm + "&checkout_monthday=" + daytm + "&group_adults=1&group_children=0&no_rooms=1&b_h4u_keep_filters=&from_sf=1"
 
 
-# In[46]:
+# In[43]:
 
 
 driver.get(urltime)
 
 
-# In[47]:
+# In[44]:
 
 
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 
 
-# In[70]:
+# In[45]:
 
 
 names = []
 prices = []
 
 
-# In[71]:
+# In[46]:
 
 
 for item in soup.findAll('span', {'class': 'sr-hotel__name'}):
         names.append(item.get_text(strip=True))
 
 
-# In[74]:
+# In[47]:
 
 
 for item in soup.findAll('div', {'class':'bui-price-display__value prco-inline-block-maker-helper'}):
     prices.append(item.get_text(strip=True))
 
 
-# In[77]:
+# In[48]:
 
 
 for price, name in zip(prices, names):
@@ -117,7 +122,7 @@ for price, name in zip(prices, names):
     print(price)
 
 
-# In[78]:
+# In[49]:
 
 
 rows = []
